86cf082944123c3838abb89422ba52fbf841728e
KEVINFLICK\quoca <quocanhbeo3010@gmail.com>
2023-08-23 18:13:03 +0700

    final ver 2 (deleting my own highscore )

--- Assets/Scripts/FOR DEBUG/DebugInput.cs
+++ Assets/Scripts/FOR DEBUG/DebugInput.cs
@@ -7,12 +7,12 @@ public class DebugInput : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        PlayerPrefs.DeleteAll();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.C)) { PlayerPrefs.DeleteAll(); }
+        
     }
 }
